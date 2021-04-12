c0d384779be4990762d391e492740229504c5391
Bodhish Thomas <bodhish@gmail.com>
2021-04-13 02:18:45 +0530

    cleanup api

--- pages/api/all_post.js
+++
@@ -1,9 +0,0 @@
-import { getAllPosts } from "../../lib/api";
-
-export default (req, res) => {
-  res
-    .status(200)
-    .json({
-      name: getAllPosts(["title", "date", "slug", "author", "excerpt"]),
-    });
-};
--- pages/api/posts/[slug].js
+++
@@ -1,8 +0,0 @@
-import { getPostBySlugJson } from "../../../lib/api";
-
-export default (req, res) => {
-  const { slug } = req.query;
-  res.status(200).json({
-    name: getPostBySlugJson(slug),
-  });
-};
