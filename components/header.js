9254f8bd934386f07e1e67ff8e029afdf9936d3c
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-04-19 23:18:31 +0530

    Fix language dropdown and categorize documents

--- components/header.js
+++ components/header.js
@@ -1,11 +1,30 @@
+import Link from "next/link";
+import Dropdown from "react-dropdown";
+import { useRouter } from "next/router";
+import "react-dropdown/style.css";
+
+const languageOptions = [
+  { label: "English", value: "en" },
+  { label: "Hindi", value: "hi" },
+];
+const defaultOption = languageOptions[0];
+
 export default function Header() {
+  const router = useRouter();
+
+  const onLanguageSelect = (option) => {
+    console.log(option);
+    router.push(`/${option.value}`);
+  };
   return (
     <header className="flex-shrink-0">
       <div className="bg-gradient-to-r from-indigo-900 to-indigo-500 ">
         <div className="max-w-4xl container mx-auto py-8 px-4 ">
           <div className="flex flex-col-reverse md:flex-row md:justify-between items-center">
             <h1 className="text-white font-bold text-xl">
-              <a href="/">CoWin | Help Center</a>
+              <Link href="/">
+                <a>CoWin | Help Center</a>
+              </Link>
             </h1>
             <div className="flex text-white">
               <a
@@ -16,7 +35,12 @@ export default function Header() {
               >
                 <span>Go to Cowin</span>
               </a>
-              <a className="px-2">English</a>
+              <Dropdown
+                options={languageOptions}
+                onChange={onLanguageSelect}
+                value={defaultOption}
+                placeholder="Language"
+              />
             </div>
           </div>
           <div className="mt-4 text-2xl text-white">
--- components/indexQuestionList.js
+++ components/indexQuestionList.js
@@ -1,17 +1,35 @@
 import Link from "next/link";
 
+function getCategories(posts) {
+  const categories = posts.map((post) => post.slug.split("/")[0]);
+  return [...new Set(categories)];
+}
+
+function getPostsByCategories(category, posts) {
+  return posts.filter((post) => post.slug.split("/")[0] == category);
+}
+
 export default function IndexQuestionList({ posts, locale }) {
   return (
     <div className="mx-auto container max-w-4xl px-6">
-      {posts.map((post) => (
-        <Link as={`${locale}/${post.slug}`} href="[locale]/[slug]">
-          <div className="mt-2 rounded bg-white p-4 shadow-md cursor-pointer">
-            <a className="text-indigo-800 font-semibold text-xl">
-              {post.title}
-            </a>
-            <div className="text-gray-600">{post.excerpt}</div>
-          </div>
-        </Link>
+      {getCategories(posts).map((category) => (
+        <div>
+          <div>{category.split("-").join(" ")}</div>
+          {getPostsByCategories(category, posts).map((post) => (
+            <Link
+              key={post.slug}
+              as={`${locale}/${post.slug}`}
+              href="[locale]/[...slug]"
+            >
+              <div className="mt-2 rounded bg-white p-4 shadow-md cursor-pointer">
+                <a className="text-indigo-800 font-semibold text-xl">
+                  {post.title}
+                </a>
+                <div className="text-gray-600">{post.excerpt}</div>
+              </div>
+            </Link>
+          ))}
+        </div>
       ))}
     </div>
   );
