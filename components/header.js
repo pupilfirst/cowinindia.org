11c476fb1110363b9d5964e9466a933d82cb1ec3
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-04-20 11:43:05 +0530

    Improve styling of page header

--- components/header.js
+++ components/header.js
@@ -2,6 +2,10 @@ import Link from "next/link";
 import Dropdown from "react-dropdown";
 import { useRouter } from "next/router";
 import "react-dropdown/style.css";
+import styles from "./header.module.css";
+
+const arrowClosed = <span className={styles.arrowClosed} />;
+const arrowOpen = <span className={styles.arrowOpen} />;
 
 const languageOptions = [
   { label: "English", value: "en" },
@@ -18,7 +22,7 @@ export default function Header() {
   };
   return (
     <header className="flex-shrink-0">
-      <div className="bg-gradient-to-r from-indigo-900 to-indigo-500 ">
+      <div className="bg-indigo-900 ">
         <div className="max-w-4xl container mx-auto py-8 px-4 ">
           <div className="flex flex-col-reverse md:flex-row md:justify-between items-center">
             <h1 className="text-white font-bold text-xl">
@@ -26,29 +30,36 @@ export default function Header() {
                 <a>CoWin | Help Center</a>
               </Link>
             </h1>
-            <div className="flex text-white">
+            <div className="flex text-white items-center">
               <a
                 target="_blank"
                 rel="noopener"
                 href="https://www.cowin.gov.in/"
-                className="px-2"
+                className="px-2 mr-3"
               >
                 <span>Go to Cowin</span>
               </a>
-              <Dropdown
-                options={languageOptions}
-                onChange={onLanguageSelect}
-                value={defaultOption}
-                placeholder="Language"
-              />
+              <div className="flex items-center">
+                <span className="mr-2"> &#x1F310;</span>
+                <Dropdown
+                  controlClassName={styles.dropdownControl}
+                  arrowClosed={arrowClosed}
+                  arrowOpen={arrowOpen}
+                  options={languageOptions}
+                  onChange={onLanguageSelect}
+                  value={defaultOption}
+                  placeholder="Language"
+                  placeholderClassName="text-white"
+                />
+              </div>
             </div>
           </div>
-          <div className="mt-4 text-2xl text-white">
+          <div className="mt-4 text-lg text-white">
             Advice and answers from the Cowin Team
           </div>
           <input
             type="text"
-            className="mt-6 w-full h-16 px-3 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg"
+            className="mt-6 w-full h-12 px-3 rounded mb-8 focus:outline-none focus:shadow-outline text-xl px-8 shadow-lg"
             placeholder="Search for articles..."
             name="search"
           ></input>
