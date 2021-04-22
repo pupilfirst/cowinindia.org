8441a8d22b78c1225676ac95bc53e0119f1260db
Mahesh Krishna Kumar <maheshcet@gmail.com>
2021-04-22 18:50:38 +0530

    Improve styling of show page

--- components/indexQuestionList.js
+++ components/indexQuestionList.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { humanize } from "../lib/utils";
 
 function getCategories(posts) {
   const categories = posts.map((post) => post.slug.split("/")[0]);
@@ -11,10 +12,12 @@ function getPostsByCategories(category, posts) {
 
 export default function IndexQuestionList({ posts, locale }) {
   return (
-    <div className="mx-auto container max-w-4xl px-6">
+    <div className="mx-auto container max-w-4xl p-6 bg-gray-300 border rounded">
       {getCategories(posts).map((category) => (
-        <div>
-          <div>{category.split("-").join(" ")}</div>
+        <div className="mb-8">
+          <div className="mb-4 text-2xl font-semibold">
+            {humanize(category)}
+          </div>
           {getPostsByCategories(category, posts).map((post) => (
             <Link
               key={post.slug}
@@ -22,10 +25,8 @@ export default function IndexQuestionList({ posts, locale }) {
               href="[locale]/[...slug]"
             >
               <div className="mt-2 rounded bg-white p-4 shadow-md cursor-pointer">
-                <a className="text-indigo-800 font-semibold text-xl">
-                  {post.title}
-                </a>
-                <div className="text-gray-600">{post.excerpt}</div>
+                <a className="text-indigo-700 text-xl">{post.title}</a>
+                <div className="text-gray-700 mt-2">{post.excerpt}</div>
               </div>
             </Link>
           ))}
--- components/showPost.js
+++ components/showPost.js
@@ -1,8 +1,8 @@
 export default function ShowPost({ post }) {
   return (
     <div className="mx-auto container max-w-4xl p-5 prose lg:prose-xl">
-      <div className="mt-2 rounded bg-white p-10 shadow-md">
-        <h2 className=" mb-5">{post.title}</h2>
+      <div className="rounded bg-white p-10 shadow-md">
+        <h2>{post.title}</h2>
         <div
           className="prose lg:prose-xl text-justify"
           dangerouslySetInnerHTML={{ __html: post.content }}
